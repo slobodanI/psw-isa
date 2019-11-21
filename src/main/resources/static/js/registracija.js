b8d935a760a93124cb95164c96e4133f93715bab
slobodan <slobodansrk@gmail.com>
2019-11-21 13:26:37 +0100

    #1_4 omoguciti izmenu podataka o pacijent na profilnoj strani

--- src/main/resources/static/js/registracija.js
+++ src/main/resources/static/js/registracija.js
@@ -16,18 +16,23 @@ $(document).ready(function(){
 		var lbo = $('input[name="lbo"]').val();
 	
 		if(password == confirmPassword){
-		$.post({
-			url: '/api/savePacijent',
-			data: JSON.stringify({username,password,confirmPassword,ime,prezime,email,brojTel,adresa,grad,drzava,lbo}),
-			contentType: 'application/json',
-			success: function(){
-				alert("Uspešno ste se registrovali.");
-				window.location = "./index.html";
-			},
-			error: function(){
-				alert("Korisnik sa ovim username-om/lbo-m već postoji.");
+			if(password.length >= 6){
+				$.post({
+					url: '/api/savePacijent',
+					data: JSON.stringify({username,password,confirmPassword,ime,prezime,email,brojTel,adresa,grad,drzava,lbo}),
+					contentType: 'application/json',
+					success: function(){
+						alert("Uspešno ste se registrovali.");
+						window.location = "./index.html";
+					},
+					error: function(){
+						alert("Korisnik sa ovim username-om/lbo-m već postoji.");
+					}
+				});
 			}
-		});
+			else {
+				alert("Sifra mora sadržati makar 6 karaktera.");
+			}
 		}
 		else{
 			alert("Unete lozinke nisu iste");
