bd9b6596bdabbe4d3ca601c3cfc49b53d751f0af
slobodan <slobodansrk@gmail.com>
2019-12-19 18:01:58 +0100

    #1_6.2.2  zakazati predefinisani pregled + EMAIL

--- src/main/java/rs/ac/uns/ftn/informatika/jpa/service/PregledService.java
+++ src/main/java/rs/ac/uns/ftn/informatika/jpa/service/PregledService.java
@@ -7,8 +7,11 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import rs.ac.uns.ftn.informatika.jpa.dto.EmailDTO;
 import rs.ac.uns.ftn.informatika.jpa.dto.PregledDTOStudent1;
 import rs.ac.uns.ftn.informatika.jpa.dto.ZavrsiPregledDTO;
 import rs.ac.uns.ftn.informatika.jpa.model.Lek;
@@ -38,6 +41,9 @@ public class PregledService {
 	
 	@Autowired
 	private PacijentService pacijentService;
+	
+	@Autowired
+	private EmailService emailService;
 	
 	public Pregled findOne(Long id) {
 		return pregledRepository.findById(id).orElseGet(null);
@@ -153,6 +159,18 @@ public class PregledService {
 		pregled.setPacijent(pacijent);
 		pregled.setZdravstveniKarton(pacijent.getZdravstveniKarton());
 		save(pregled);
+		
+		EmailDTO emailDTO = new EmailDTO(pacijent.getId().intValue(), "Uspesno zakazan pregled", "Uspesno ste zakazali pregled preko profila klinike", "");
+
+		try 
+		{
+			emailService.sendNotificaitionAsync(emailDTO);
+		}
+		catch( Exception e )
+		{
+//			logger.info("Greska prilikom slanja emaila: " + e.getMessage());
+			System.out.println("### Greska prilikom slanja mail-a! ###");
+		}
 		
 		return true;
 	}
